a36df0f1b20af888ab2ebe9487df74fc54bc9d29
Ruslan Kladko <rkladko@gmail.com>
2025-04-04 19:21:55 +0200

    task1 add benchmark

--- task1_test.go
+++ task1_test.go
@@ -10,6 +10,9 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
+	"unsafe"
+	"math/rand"
 )
 
 func read_file(from_file string) ([]string, error) {
@@ -76,4 +79,39 @@ func Test_testCondition(t *testing.T) {
 	}
 }
 
-// todo: benchmark
+/**
+ * Random string generator
+ */
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+    letterIdxBits = 6                    // 6 bits to represent a letter index
+    letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+    letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func RandStringBytesMaskImprSrcUnsafe(n int) string {
+    b := make([]byte, n)
+    // A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+    for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+        if remain == 0 {
+            cache, remain = src.Int63(), letterIdxMax
+        }
+        if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+            b[i] = letterBytes[idx]
+            i--
+        }
+        cache >>= letterIdxBits
+        remain--
+    }
+
+    return *(*string)(unsafe.Pointer(&b))
+}
+
+func Benchmark_testCondition(b *testing.B) {
+    for i := 0; b.Loop(); i++ {
+        testCondition(RandStringBytesMaskImprSrcUnsafe(i+7))
+    }
+}
